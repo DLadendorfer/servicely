483238edb4225581e28a39453f7fefc2505ba419
Daniel Ladendorfer <daniel.ladendorfer@stiwa.com>
2025-02-27 09:38:10 +0100

    Log adjustments

--- src/main/java/com/aero/servicely/core/utils/LogPanelAppender.java
+++ src/main/java/com/aero/servicely/core/utils/LogPanelAppender.java
@@ -4,10 +4,15 @@ import java.awt.*;
 import java.io.Serializable;
 import javax.swing.*;
 import javax.swing.text.*;
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.*;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginElement;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 
+@Plugin(name = "GUI", category = "Core", elementType = "appender", printObject = true)
 public class LogPanelAppender extends AbstractAppender {
   private static JTextPane logPane;
   private static StyledDocument doc;
@@ -21,6 +26,23 @@ public class LogPanelAppender extends AbstractAppender {
     doc = textPane.getStyledDocument();
   }
 
+  @PluginFactory
+  public static LogPanelAppender createAppender(
+      @PluginAttribute("name") final String name,
+      @PluginElement("Filters") final Filter filter,
+      @PluginElement("Layout") Layout<?> layout) {
+
+    if (name == null) {
+      LOGGER.error("No name provided for StubAppender");
+      return null;
+    }
+
+    if (layout == null) {
+      layout = PatternLayout.createDefaultLayout();
+    }
+    return new LogPanelAppender(name, filter, layout);
+  }
+
   @Override
   public void append(LogEvent event) {
     if (logPane == null) return;
@@ -29,36 +51,21 @@ public class LogPanelAppender extends AbstractAppender {
         () -> {
           try {
             String level = event.getLevel().name();
-            String message = new String(getLayout().toByteArray(event));
+            if (getLayout() != null) {
+              String message = new String(getLayout().toByteArray(event));
 
-            Style style = doc.getStyle(level);
-            if (style == null) {
-              style = doc.addStyle(level, null);
-              StyleConstants.setForeground(style, getColor(event.getLevel()));
+              Style style = doc.getStyle(level);
+              if (style == null) {
+                style = doc.addStyle(level, null);
+                StyleConstants.setForeground(style, logPane.getForeground());
+              }
+
+              doc.insertString(doc.getLength(), message, style);
+              logPane.setCaretPosition(doc.getLength());
             }
-
-            doc.insertString(doc.getLength(), message, style);
-            logPane.setCaretPosition(doc.getLength());
           } catch (BadLocationException e) {
             e.printStackTrace();
           }
         });
-  }
-
-  private static Color getColor(Level level) {
-    switch (level.name()) {
-      case "ERROR":
-        return Color.RED;
-      case "WARN":
-        return Color.ORANGE;
-      case "INFO":
-        return Color.BLACK;
-      case "DEBUG":
-        return Color.BLUE;
-      case "TRACE":
-        return Color.GRAY;
-      default:
-        return Color.BLACK;
-    }
   }
 }
--- src/main/java/com/aero/servicely/ui/panels/LogPanel.java
+++ src/main/java/com/aero/servicely/ui/panels/LogPanel.java
@@ -1,14 +1,9 @@
 package com.aero.servicely.ui.panels;
 
-import com.aero.servicely.core.utils.LogPanelAppender;
+import com.aero.servicely.ui.UiConstants;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.text.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Logger;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.LoggerConfig;
 
 public class LogPanel extends JPanel {
   private final JTextPane logPane;
@@ -18,21 +13,19 @@ public class LogPanel extends JPanel {
     setLayout(new BorderLayout());
 
     logPane = new JTextPane();
+    logPane.setPreferredSize(new Dimension(getPreferredSize().width, 150));
     logPane.setEditable(false);
-    logPane.setFont(new Font("Consolas", Font.PLAIN, 12));
+    logPane.setFont(UiConstants.INSTANCE.getDEFAULT_FONT());
 
     doc = logPane.getStyledDocument();
     addStyles();
 
     JScrollPane scrollPane = new JScrollPane(logPane);
     add(scrollPane, BorderLayout.CENTER);
-
-    // Register Log4j2 custom appender
-    registerAppender();
   }
 
   private void addStyles() {
-    addStyle("ERROR",  new Color(182, 51, 51));
+    addStyle("ERROR", new Color(182, 51, 51));
     addStyle("WARN", new Color(182, 51, 51));
     addStyle("INFO", getForeground());
     addStyle("DEBUG", Color.BLUE);
@@ -43,17 +36,4 @@ public class LogPanel extends JPanel {
     Style style = doc.addStyle(level, null);
     StyleConstants.setForeground(style, color);
   }
-
-  private void registerAppender() {
-    LogPanelAppender appender = new LogPanelAppender("GUI", null, null);
-    LogPanelAppender.setLogPane(logPane);
-
-    Logger rootLogger = (Logger) LogManager.getRootLogger();
-    Configuration config = rootLogger.getContext().getConfiguration();
-    LoggerConfig loggerConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
-
-    loggerConfig.addAppender(appender, null, null);
-    Configurator.reconfigure();
-  }
-
 }
